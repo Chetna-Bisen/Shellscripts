ae52957ebb9aaf8faece412dd082b130f191de3e
Chetna-Bisen <bisen.chetna044@gmail.com>
2020-10-24 02:35:34 +0530

    Calculating Wages till a condition

---
+++ UC6_CalculatingWageTillCondition.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+
+#Constants
+isFullTime=1
+isPartTime=2
+empWagePerHr=20
+numWorkingDays=20
+maxWorkingHrs=100
+
+#Variables
+totalEmpHrs=0
+totalWorkingDays=0
+
+#Calculating Wages for a month
+
+while [[ $totalEmpHrs -lt $maxWorkingHrs && $totalWorkingDays -lt $numWorkingDays ]]
+do
+    ((totalWorkingDays++))
+    empCheck=$(( RANDOM%3 ))
+    case $empCheck in
+         $isFullTime)
+            empHr=16
+              ;;
+         $isPartTime)
+            empHr=8
+              ;;
+         *)
+            empHr=0
+   esac
+
+totalEmpHrs=$(($totalEmpHrs+$empHr))
+empSalary=$(($empWagePerHr*$empHr))   #Calculating Employee Wage
+totalSalary=$(($totalSalary+$empSalary))
+done
